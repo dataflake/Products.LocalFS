0d10963dea0aaa8dca23e49349ec0f563f9b7c50
Jens Vagelpohl <jens@plyp.com>
2022-09-20 10:46:58 +0200

    - factor out methods and definitions from LocalFS module

--- src/Products/LocalFS/LocalFS.py
+++ src/Products/LocalFS/LocalFS.py
@@ -58,44 +58,42 @@ import App
 import OFS
 import Persistence
 from App.Dialogs import MessageDialog
-from App.Extensions import getObject
 from App.FactoryDispatcher import ProductDispatcher
 from App.special_dtml import HTMLFile
 from DateTime import DateTime
 from OFS.CopySupport import CopyError
 from OFS.CopySupport import _cb_decode
 from OFS.CopySupport import _cb_encode
-from OFS.Image import Pdata
 from OFS.role import RoleManager
-from Products.PageTemplates.ZopePageTemplate import ZopePageTemplate
-from Products.PythonScripts.PythonScript import PythonScript
 from TreeDisplay.TreeTag import encode_str
 from webdav.NullResource import LockNullResource
 from zExceptions import BadRequest
 from zExceptions import Forbidden
 from zExceptions import NotFound
 from zExceptions import Unauthorized
-from ZODB.utils import p64
-from zope.contenttype import find_binary
 from ZPublisher.HTTPResponse import HTTPResponse
 
-from .StreamingFile import StreamingFile
-from .StreamingFile import StreamingImage
+from .exceptions import DeleteError
+from .exceptions import RenameError
+from .exceptions import UploadError
+from .utils import _create_ob
+from .utils import _get_content_type
+from .utils import _iconmap2list
+from .utils import _iswin32
+from .utils import _list2iconmap
+from .utils import _list2typemap
+from .utils import _marker
+from .utils import _save_ob
+from .utils import _set_content_type
+from .utils import _set_timestamp
+from .utils import _test_read
+from .utils import _typemap2list
+from .utils import _unknown
+from .utils import absattr
+from .utils import bad_id
+from .utils import sanity_check
+from .utils import valid_id
 
-
-class UploadError(Exception):
-    pass
-
-
-class RenameError(Exception):
-    pass
-
-
-class DeleteError(Exception):
-    pass
-
-
-_iswin32 = (sys.platform == 'win32')
 
 if (_iswin32):
     try:
@@ -104,8 +102,6 @@ if (_iswin32):
         pass
     unc_expr = re.compile(r'(\\\\[^\\]+\\[^\\]+)(.*)')
 
-_test_read = 1024 * 8
-_unknown = '(unknown)'
 
 ############################################################################
 # The process of determining the content-type involves the following steps.
@@ -122,27 +118,6 @@ _unknown = '(unknown)'
 #    shouldn't have and change the type back to 'text/plain'. This
 #    also happens in _set_content_type.
 ############################################################################
-
-
-def _get_content_type(ext, _type_map):
-    """_get_content_type"""
-    try:
-        return _type_map[ext]
-    except KeyError:
-        return (None, None)
-
-
-def _set_content_type(ob, content_type, data):
-    """_set_content_type"""
-    if content_type:
-        ob.content_type = content_type
-
-    if hasattr(ob, 'content_type') and ob.content_type == 'text/html':
-        if content_type == 'text/html':
-            return
-        data = data.strip().lower()
-        if data[:6] != '<html>' and data[:14] != '<!doctype html':
-            ob.content_type = 'text/plain'
 
 
 _types = {
@@ -168,40 +143,6 @@ _types = {
         'Products.StructuredDocument.StructuredDocument.StructuredDocument'),
     '.xml': ('text/xml', 'LocalFS.Factory.XMLDocumentFactory'),
 }
-_typemap_error = 'Error parsing type map: "%s"'
-
-
-def _list2typemap(types_list):
-    """_list2typemap"""
-    if not types_list:
-        return
-    types_mapping = {}
-    for i in types_list:
-        if i:
-            try:
-                e, t = i.split()
-                c = ''
-            except ValueError:
-                try:
-                    e, t, c = re.split('[ \t]+', i, 2)
-                except ValueError:
-                    raise ValueError(_typemap_error % i)
-            types_mapping[e] = (t, c)
-    return types_mapping
-
-
-def _typemap2list(types_mapping):
-    """_typemap2list"""
-    res = []
-    keys = sorted(types_mapping.keys())
-    for k in keys:
-        v = types_mapping[k]
-        if isinstance(v, tuple):
-            res.append(''.join((k, v[0], v[1])))
-        else:
-            res.append(''.join((k, v)))
-    return res
-
 
 _icons = {
     'directory': 'dir.gif',
@@ -240,345 +181,6 @@ _icons = {
 _icon_base = 'misc_/LocalFS/'
 for k, v in _icons.items():
     _icons[k] = _icon_base + v
-_iconmap_error = "Error parsing icon map: '%s'"
-
-
-def _list2iconmap(icons_list):
-    """_list2iconmap"""
-    if not icons_list:
-        return
-    icons_mapping = {}
-    for i in icons_list:
-        if i:
-            try:
-                k, v = i.split()
-            except ValueError:
-                raise ValueError(_iconmap_error % i)
-            icons_mapping[k] = v
-    return icons_mapping
-
-
-def _iconmap2list(icons_mapping):
-    """_iconmap2list"""
-    res = []
-    for key in sorted(icons_mapping.keys()):
-        res.append(''.join((key, icons_mapping[key])))
-    return res
-
-
-def _create_ob(id, file, path, _type_map):
-    """_create_ob"""
-    ob = None
-    ext = os.path.splitext(path)[-1]
-    t, c = _get_content_type(ext, _type_map)
-    if c is not None:
-        ob = _create_builtin_ob(c, id, file, path)
-        if ob is None:
-            ob = _create_ob_from_function(c, id, file, path)
-        if ob is None:
-            ob = _create_ob_from_factory(c, id, file, path)
-    if ob is None:
-        ob = _wrap_ob(_create_File(id, file), path)
-    file.seek(0)
-    ob.__doc__ = 'LocalFile'
-    _set_content_type(ob, t, file.read(_test_read))
-    return ob
-
-
-def _create_DTMLMethod(id, file):
-    """_create_DTMLMethod"""
-    return OFS.DTMLMethod.DTMLMethod(file.read(), __name__=id)
-
-
-def _create_DTMLDocument(id, file):
-    """_create_DTMLDocument"""
-    return OFS.DTMLDocument.DTMLDocument(file.read(), __name__=id)
-
-
-def _create_Image(id, file):
-    """_create_Image"""
-    return StreamingImage(id, '', file)
-
-
-def _create_File(id, file):
-    """_create_File"""
-    return StreamingFile(id, '', file)
-
-
-def _create_ZPT(id, file):
-    """_create_ZPT"""
-    return ZopePageTemplate(id, file, content_type='text/html')
-
-
-def _create_PythonScript(id, file):
-    """_create_PythonScript"""
-    ob = PythonScript(id)
-    ob.write(file.read())
-    return ob
-
-
-_builtin_create = {
-    'PythonScript': _create_PythonScript,
-    'DTMLMethod': _create_DTMLMethod,
-    'DTMLDocument': _create_DTMLDocument,
-    'Image': _create_Image,
-    'File': _create_File,
-    'PageTemplate': _create_ZPT,  # ***SmileyChris
-}
-
-
-def _create_builtin_ob(c, id, file, path):
-    try:
-        f = _builtin_create[c]
-        return _wrap_ob(f(id, file), path)
-    except Exception:
-        pass
-
-
-def _create_ob_from_function(c, id, file, path):
-    try:
-        i = c.rindex('.')
-        m, c = c[:i], c[i+1:]
-        m = __import__(m, globals(), locals(), (c,))
-        c = getattr(m, c)
-        f = getattr(c, 'createSelf').__func__
-        if f.__code__.co_varnames == ('id', 'file'):
-            return _wrap_ob(f(id, file), path)
-    except Exception:
-        pass
-
-
-def _create_ob_from_factory(c, id, file, path):
-    try:
-        i = c.rindex('.')
-        m, c = c[:i], c[i+1:]
-        c = getObject(m, c)
-        f = c()
-        ob = _wrap_ob(f(id, file), path)
-        ob.__factory = f
-        return ob
-    except Exception:
-        pass
-
-
-class Wrapper:
-    """Mix-in class used to save object changes."""
-    _local_path = None
-    # Create a global and persistent lock table
-    _dav_writelocks = {}
-    # The object itself is not persistent and cannot be stored
-    _p_changed = 0
-
-    def bobobase_modification_time(self):
-        """ bobobase_modification_time """
-        t = os.stat(self._local_path)[stat.ST_MTIME]
-        return DateTime(t)
-
-    def __repr__(self):
-        """ __repr__ """
-        c = self.__class__.__bases__[-1].__name__
-        return '<%s ObjectWrapper instance at %8X>' % (c, id(self))
-
-    def wl_lockmapping(self, killinvalids=0, create=0):
-        """ Overwrite the default method of LockableItem """
-        locks = self._dav_writelocks.get(self._local_path)
-        if locks is None:
-            locks = {}
-            if create:
-                # Store it in persistent lock table
-                self._dav_writelocks[self._local_path] = locks
-        elif killinvalids:
-            # Delete invalid locks
-            for token, lock in locks.items():
-                if not lock.isValid():
-                    del locks[token]
-            if not locks and not create:
-                # Remove empty lock table
-                del self._dav_writelocks[self._local_path]
-        return locks
-
-
-_wrapper_method = '''def %(name)s %(arglist)s:
-    """Wrapper for the %(name)s method."""
-    r = apply(self.__class__.__bases__[-1].%(name)s, %(baseargs)s)
-    try: _save_ob(self, self._local_path)
-    except ValueError: pass
-    return r
-'''
-
-_wrappers = {}
-
-
-def _get_wrapper(c):
-    try:
-        return _wrappers[c]
-    except KeyError:
-        class ObjectWrapper(Wrapper, c):
-            pass
-
-        _wrap_method(ObjectWrapper, 'manage_edit')
-        _wrap_method(ObjectWrapper, 'manage_upload')
-        _wrap_method(ObjectWrapper, 'pt_edit')  # PT ZMI editing
-        _wrap_method(ObjectWrapper, 'write')  # PythonScript
-        _wrap_method(ObjectWrapper, 'ZBindingsHTML_editAction')  # PythonScript
-        _wrap_method(ObjectWrapper, 'PUT')
-        # Remove management options that cannot be used
-        manage_options = []
-        for opt in ObjectWrapper.manage_options:
-            if opt['label'] not in ('Properties', 'Security', 'Undo',
-                                    'Ownership', 'Interfaces', 'Proxy',
-                                    'History'):
-                manage_options.append(opt)
-        ObjectWrapper.manage_options = tuple(manage_options)
-        _wrappers[c] = ObjectWrapper
-        return ObjectWrapper
-
-
-def _wrap_method(c, name):
-    try:
-        m = getattr(c.__bases__[-1], name)
-    except AttributeError:
-        return
-    f = m.__func__
-    a = list(f.__code__.co_varnames)[:f.__code__.co_argcount]
-    d = f.__defaults__ or ()
-    arglist = []
-    baseargs = []
-    while (len(a) > len(d)):
-        arglist.append(a[0])
-        baseargs.append(a[0])
-        del a[0]
-    for i in range(len(a)):
-        arglist.append('%s=%s' % (a[i], repr(d[i])))
-        baseargs.append(a[i])
-    arglist = '(' + ','.join(arglist) + ')'
-    baseargs = '(' + ','.join(baseargs) + ')'
-    d = {}
-    # exec _wrapper_method % vars() in globals(), d
-    exec(_wrapper_method % vars(), globals(), d)
-    setattr(c, name, d[name])
-
-
-def _wrap_ob(ob, path):
-    c = ob.__class__
-    n = ob.__name__
-    if hasattr(c, '__basicnew__'):
-        c = _get_wrapper(c)
-        d = ob.__dict__
-        ob = c.__basicnew__()
-        ob.__dict__.update(d)
-    else:
-        c = _get_wrapper(c)
-        ob.__class__ = c
-    ob._local_path = path
-    ob.__name__ = n
-    ob._p_oid = path
-    return ob
-
-
-def _save_DTML(ob, path):
-    f = open(path, 'w')
-    try:
-        f.write(ob.read())
-    finally:
-        f.close()
-
-
-def _save_File(ob, path):
-    if isinstance(ob.data, Pdata):
-        f = open(path, 'wb')
-        f.write(str(ob.data))
-        f.close()
-    else:
-        if find_binary(ob.data) >= 0:
-            f = open(path, 'wb')
-        else:
-            f = open(path, 'w')
-        f.write(ob.data)
-        f.close()
-
-
-def _save_Folder(ob, path):
-    os.mkdir(path)
-
-
-_builtin_save = {
-    'Script (Python)': _save_DTML,
-    'DTML Method': _save_DTML,
-    'DTML Document': _save_DTML,
-    'Image': _save_File,
-    'File': _save_File,
-    'Folder': _save_Folder,
-    'Page Template': _save_DTML,  # ***SmileyChris
-}
-
-
-def _save_builtin_ob(ob, path):
-    try:
-        f = _builtin_save[ob.meta_type]
-        f(ob, path)
-        return 1
-    except KeyError:
-        pass
-
-
-def _save_ob_with_function(ob, path):
-    try:
-        ob.saveSelf(path)
-        return 1
-    except Exception:
-        pass
-
-
-def _save_ob_with_factory(ob, path):
-    try:
-        ob.__factory.save(ob, path)
-        return 1
-    except Exception:
-        pass
-
-
-def _save_ob(ob, path):
-    s = _save_builtin_ob(ob, path)
-    if not s:
-        s = _save_ob_with_function(ob, path)
-    if not s:
-        s = _save_ob_with_factory(ob, path)
-    if not s:
-        raise TypeError("Cannot save files of type '%s'." % ob.meta_type)
-
-
-def _set_timestamp(ob, path):
-    t = os.stat(path)[stat.ST_MTIME]
-    ob._p_serial = p64(t)
-
-
-_marker = []
-
-
-def valid_id(id):
-    if id == os.curdir or id == os.pardir or id[0] == '_':
-        return 0
-    return 1
-
-
-bad_id = re.compile('[^a-zA-Z0-9-_~,. ]').search
-
-
-def absattr(attr):
-    if callable(attr):
-        return attr()
-    return attr
-
-
-def sanity_check(c, ob):
-    # This is called on cut/paste operations to make sure that
-    # an object is not cut and pasted into itself or one of its
-    # subobjects, which is an undefined situation.
-    dest = c.basepath
-    src = ob._local_path
-    if dest[:len(src)] != src:
-        return 1
 
 
 class LocalDirectory(OFS.CopySupport.CopyContainer, App.Management.Navigation,
